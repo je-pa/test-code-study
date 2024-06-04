65b64ac6b30826237effe3e1e8d37dc7a711a9a1
je-pa <jieun0502@gmail.com>
2024-06-05 03:53:41 +0900

    🎉 Begin

---
+++ src/test/java/com/zerobase/testcodestudy/TestCodeStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zerobase.testcodestudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestCodeStudyApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
